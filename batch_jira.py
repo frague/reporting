fcc11d81be010d6349fc25d10586a6b575a60433
Nick S. Bogdanov <nbogdanov@griddynamics.com>
2010-05-05 19:46:13 +0400

    Batch jira processor

--- batch_jira.py
+++ batch_jira.py
@@ -1,44 +1,84 @@
 from rabbithole import *
 
 
+class JiraIssue:
+	global soap, jiraAuth
 
-multiline = re.compile('"([^"]+)"', re.MULTILINE)
+	def __init__(self):
+		pass
 
-def reNewLine(matchObj):
-	return matchObj.group(1).replace("\n", "\\\\")
+	def Number(self):
+		if self.IsNotEmpty():
+			return int(re.sub("[^0-9]", "", self.key))
+		else:
+			return 0
 
-def deNewLine(text):
-	return text.replace("\\\\", " \\n ").replace('"', "")
+	def Parse(self, line):
+		for key in line._keys():
+			setattr(self, key, line[key])
+	
+	def Clear(self):
+		self.id = 0
+		self.key = ""
 
-def makePriority(p):
-	if p == "P1":
-		return "Critical"
-	return "Major"
+	def IsNotEmpty(self):
+		return self.key and self.id
 
-tasks = multiline.sub(reNewLine, ReadFile("Tasks.txt"))
+	def Update(self, changes):
+		if self.IsNotEmpty():
+			soap.updateIssue(jiraAuth, self.key, changes)
 
-'''soap = SOAPpy.WSDL.Proxy(config["jira_soap"])
+
+
+ProfileNeeded()
+
+soap = SOAPpy.WSDL.Proxy(config["jira_soap"])
 jiraAuth = soap.login(config["jira"]["user"], config["jira"]["password"])
-newissue = soap.createIssue(jiraAuth, {"project": "TORS", "type": 1, "summary": "jdhfkjshdk"})'''
+
+filterName = "CodeHardening2Report"
+issue = JiraIssue()
+
+issues = soap.getIssuesFromFilter(jiraAuth, "11198")
+
+for i in issues:
+	issue.Parse(i)
+	n = issue.Number()
+
+	print "%s: %s" % (issue.key, issue.status)
+
+	if (issue.status != "6"):
+		if n >= 645 and n <=716:
+			issue.Update([{"id": "fixVersion", "values": ["10721", "10725"]}])
+
+		if n > 716 and n <=762:
+			issue.Update([{"id": "fixVersion", "values": ["10721", "10726"]}])
+
+'''if re.search(" same ", issue.summary):
+		print "-- %s" % issue.summary
+		issue.Update([{"id": "priority", "values": "3"}, {"id": "assignee", "values": "tgautier"}])'''
+
+#		issue.Update([{"id": "priority", "values": "3"}])
 
 
+'''
 
+[
+059.        {"id": "summary", "values": ['[Updated] Issue created with Python'] },
+060. 
+061.        # Change issue type to 'New feature'
+062.        {"id":"issuetype", "values":'2'},
+063. 
+064.        # Setting a custom field. The id (10010) is discoverable from
+065.        # the database or URLs in the admin section
+066. 
+067.        {"id": "customfield_10010", "values": ["Random text set in updateIssue method"] },
+068. 
+069.        {"id":"fixVersions", "values":['10331']},
+070.        # Demonstrate setting a cascading selectlist:
+071.        {"id": "customfield_10061", "values": ["10098"]},
+072.        {"id": "customfield_10061_1", "values": ["10105"]},
+073.        {"id": "duedate", "values": datetime.date.today().strftime("%d-%b-%y")}
+074. 
+075.        ]
 
-
-
-
-
-
-
-
-#i = 0
-for line in re.split("\n", tasks):
-	columns = line.split("	")
-	if len(columns) == 7 and columns[2] != "":
-		[Sig, Subject, Actions, ExpectedResult, Priority, Estimate, Comments] = columns
-#		i = i + 1
-#		if i < 5:
-#			newissue = soap.createIssue(jiraAuth, {"project": "TORS", "type": "Task", "fixVersions": "Code Hardening", "summary": "%s: %s" % (Sig, Subject), "description": "*Actions*:\n%s\n\n*Expected Result*:\n%s\n\n*Estimate*: %s\n\n*Comments*: %s" % (deNewLine(Actions), deNewLine(ExpectedResult), Estimate, deNewLine(Comments)), "priority": makePriority(Priority), "estimate": Estimate})
-#			newissue = soap.createIssue(jiraAuth, {"project": "TORS", "type": 1, "fixVersions": "Code Hardening", "summary": "jdhfkjshdk"})
-		print "--- %s: %s, %s, %s, %s, (%s) %s" % (Sig, Subject, deNewLine(Actions), deNewLine(ExpectedResult), makePriority(Priority), Estimate, deNewLine(Comments))
-#		GetJira({"action": "createIssue", "project": "TORS", "type": "Task", "fixVersions": "Code Hardening", "summary": "%s: %s" % (Sig, Subject), "description": "*Actions*: \\n %s \\n  \\n *Expected Result*: \\n %s \\n  \\n *Estimate*: %s \\n  \\n *Comments*: %s" % (deNewLine(Actions), deNewLine(ExpectedResult), Estimate, deNewLine(Comments)), "priority": makePriority(Priority)})
+'''
