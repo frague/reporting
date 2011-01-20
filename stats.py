845a380f4c154472fa47fe3add08e846b1511ee1
Nick S. Bogdanov <nbogdanov@griddynamics.com>
2011-01-20 19:15:59 +0300

    KPI (key project indexes) calculation templates and logic updated to contain last values table

--- stats.py
+++ stats.py
@@ -12,6 +12,7 @@ class ExternalData:
 	def __init__(self, wikiServer, wikiToken):
 		self.wikiServer = wikiServer
 		self.wikiToken = wikiToken
+		self.PostFix = ""
 		self.Init()
 
 	def Init(self):
@@ -25,42 +26,49 @@ class ExternalData:
 		return result
 		
 	def FetchData(self):
-		print "[!] No fetching logic is defined!"
+		print "(!) No fetching logic is defined!"
 		return {}
 
-	def MakeContent(self, data):
-		print "[!] No content formatting logic is defined!"
-		return ""
+	def GetLastValues(self, data):
+		return data[sorted(data.keys(), reverse=True)[0]]
+
+	def BuildLastValuesTable(self, data):
+		headers = "|| "
+		columns = "| "
+		lastValues = self.GetLastValues(data)
+		for key, value in sorted(lastValues.iteritems(), key=lambda (k,v): (v,k)):
+			headers += "%s || " % key
+			columns += "%s%s | " % (value, self.PostFix)
+		print "(i) Last values: %s" % lastValues
+		return "%s\n%s" % (headers, columns)
+
+   	def MakeContent(self, data):
+		print "(!) Updating wiki %s report page (with no notification)" % self.PageName
+		return FillTemplate(GetTemplate(self.Template), {"##UPDATED##": datetime.datetime.today().strftime("%b %d, %Y (%H:%M)"), "##LASTVALUES##": self.BuildLastValuesTable(data), "##CHART##": MakeWikiProgressChart(data)})
 
 	def UpdatePage(self, content):
 #		print "[x] Saving is disabled for now...\n"
 #		return
 
 		if not self.PageName:
-			print "[!] No page name is set!"
+			print "(!) No page name is set!"
 			return
 
 		page = self.wikiServer.confluence1.getPage(self.wikiToken, config["project_space"], self.PageName)
 		page["content"] = content
 		self.wikiServer.confluence1.updatePage(self.wikiToken, page, {"minorEdit": True})
 
-		print "[+] Page %s has been updated." % self.PageName
+		print "(i) Page %s has been updated." % self.PageName
 
 	def Run(self):
 		print "======= Running %s report creation ==========================================" % self.PageName
 		data = self.FetchData()
 		if not data:
-			print "[x] Data fetching error - exiting!"
+			print "(!) Data fetching error - exiting!"
 			return
 
 		self.UpdatePage(self.MakeContent(data))
 		print "\n"
-
-
-class HudsonReport(ExternalData):
-   	def MakeContent(self, data):
-		print "[+] Updating wiki %s report page (with no notification)" % self.PageName
-		return FillTemplate(GetTemplate(self.Template), {"##UPDATED##": datetime.datetime.today().strftime("%b %d, %Y (%H:%M)"), "##CHART##": MakeWikiProgressChart(data)})
 
 
 ######################################################################################
@@ -72,13 +80,14 @@ class TestsCoverage(ExternalData):
 		self.Template = "coverage"
 		self.PageName = "Code Coverage"
 		self.CacheName = "cobertura"
+		self.PostFix = "%"
 
 	def collectStat(self, matchObj):
 		measure = matchObj.group(1)
 		value = matchObj.group(2)
 		self.stats[measure] = value
 
-		print "- %s = %s%%" % (measure, value)
+		#print "- %s = %s%%" % (measure, value)
 		return ""
 
 	def FetchData(self):
@@ -88,16 +97,20 @@ class TestsCoverage(ExternalData):
 		percents.sub(self.collectStat, self.WgetPage(self.Url))
 		return SaveUpdates(config["project_abbr"], self.CacheName, self.stats)
 
-	def MakeContent(self, data):
-		headers = "|| "
-		columns = "| "
-		for key, value in sorted(data.iteritems(), key=lambda (k,v): (v,k)):
-			headers += "%s || " % key
-			columns += "%s%% | " % value
 
-		print "[+] Updating wiki tests coverage page (with no notification)"
+######################################################################################
+# Hudson standard report table
 
-		return FillTemplate(GetTemplate(self.Template), {"##UPDATED##": datetime.datetime.today().strftime("%b %d, %Y (%H:%M)"), "##HEADERS##": headers, "##COLUMNS##": columns, "##COVERAGECHART##": MakeWikiProgressChart(data)})
+class HudsonReport(ExternalData):
+	def FetchData(self):
+		markup = GetMatchGroup(self.WgetPage(self.Url), re.compile("<table[^>]*id=\"analysis\.summary\"[^>]*>(([^<]|<[^/]|</[^t]|</t[^a])+)</table>"), 1)
+
+		if not markup:
+			return False
+
+		stats = ParseHeadedTable(markup, True)
+		return SaveUpdates(config["project_abbr"], self.CacheName, stats[0])
+
 
 
 ######################################################################################
@@ -110,18 +123,6 @@ class PMDReport(HudsonReport):
 		self.PageName = "PMD"
 		self.CacheName = "pmd"
 
-	def FetchData(self):
-		markup = GetMatchGroup(self.WgetPage(self.Url), re.compile("<table[^>]*id=\"analysis\.summary\"[^>]*>(([^<]|<[^/]|</[^t]|</t[^a])+)</table>"), 1)
-
-		if not markup:
-			return False
-
-		stats = ParseHeadedTable(markup, True)
-
-		print stats[0]
-
-		return SaveUpdates(config["project_abbr"], self.CacheName, stats[0])
-
 
 ######################################################################################
 # Code coverage w/ tests (Cobertura-based)
@@ -133,21 +134,11 @@ class FindBugsReport(HudsonReport):
 		self.PageName = "FindBugs"
 		self.CacheName = "findbugs"
 
-	def FetchData(self):
-		markup = GetMatchGroup(self.WgetPage(self.Url), re.compile("<table[^>]*id=\"analysis\.summary\"[^>]*>(([^<]|<[^/]|</[^t]|</t[^a])+)</table>"), 1)
-
-		if not markup:
-			return False
-
-		stats = ParseHeadedTable(markup, True)
-		print stats[0]
-		return SaveUpdates(config["project_abbr"], self.CacheName, stats[0])
-
 
 ######################################################################################
 # Code coverage w/ tests (Cobertura-based)
 
-class TestsRunReport(HudsonReport):
+class TestsRunReport(ExternalData):
 	def Init(self):
 		self.Url = config["cobertura_log"]
 		self.Template = "tests"
