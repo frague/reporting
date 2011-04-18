433a2eb4bf81009bbf2d525e18dfea56f27c8a6e
Nick S. Bogdanov <nbogdanov@griddynamics.com>
2011-04-18 20:01:41 +0400

    Batches for clis simplified

--- confluence.bat
+++ confluence.bat
@@ -4,7 +4,8 @@ rem Comments
 rem - Customize for your installation, for instance you might want to add default parameters like the following:
 rem   java -jar release/confluence-cli-1.5.0.jar --server http://my-server --user automation --password automation %*
 
-java -jar D:\dev\reporting\confluence_cli\release\confluence-cli-1.5.0.jar %*
+rem java -jar D:\dev\reporting\confluence_cli\release\confluence-cli-1.5.0.jar %*
+java -jar jars/confluence-cli-1.5.0.jar %*
 
 rem Exit with the correct error level.
 EXIT /B %ERRORLEVEL%
--- jira.bat
+++ jira.bat
@@ -4,7 +4,9 @@ rem Comments
 rem - Customize for your installation, for instance you might want to add default parameters like the following:
 rem   java -jar release/jira-cli-1.5.0.jar --server http://my-server --user automation --password automation %*
 
-java -jar D:\dev\reporting\jira_cli\release\jira-cli-1.5.0.jar %*
+rem java -jar D:\dev\reporting\jira_cli\release\jira-cli-1.5.0.jar %*
+rem java -jar "C:\Program Files\Java\jars\jira-cli-1.5.0.jar" %*
+java -jar jars/jira-cli-1.5.0.jar %*
 
 rem Exit with the correct error level.
 EXIT /B %ERRORLEVEL%
